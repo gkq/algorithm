724bcf6fc9ee89223ed585f10a6a70f7148a889c
gkq <gukunqi@gmai.com>
2016-05-01 21:54:16 +0800

    add binary tree order

---
+++ Array/src/main/org/sjtu/gkq/BinaryTree.java
@@ -0,0 +1,140 @@
+package org.sjtu.gkq;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class BinaryTree {
+    public TreeNode root;
+
+    class  TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+            left = right = null;
+        }
+    }
+
+    public BinaryTree() {
+        this.root = new TreeNode(-1);
+    }
+
+    public void push(int data) {
+        buildTree(root, data);
+    }
+
+    private void buildTree(TreeNode node, int data) {
+        if(node == null) {
+            node = new TreeNode(data);
+        }
+        else if(node.val > data) {
+            if(node.left == null){
+                node.left = new TreeNode(data);
+            }
+            else buildTree(node.left, data);
+        }
+        else {
+            if(node.right == null){
+                node.right = new TreeNode(data);
+            }
+            else buildTree(node.right, data);
+        }
+    }
+
+    public ArrayList preOrder(TreeNode node) {
+        ArrayList res = new ArrayList();
+        if(node != null) {
+            res.add(node.val);
+            res.addAll(preOrder(node.left));
+            res.addAll(preOrder(node.right));
+        }
+        return res;
+    }
+
+    public ArrayList inOrder(TreeNode node){
+        ArrayList res = new ArrayList();
+        if(node != null){
+            res.addAll(preOrder(node.left));
+            res.add(node.val);
+            res.addAll(preOrder(node.right));
+        }
+        return res;
+    }
+
+    public ArrayList postOrder(TreeNode node){
+        ArrayList res = new ArrayList();
+        if(node != null){
+            res.addAll(preOrder(node.left));
+            res.addAll(preOrder(node.right));
+            res.add(node.val);
+        }
+        return res;
+    }
+
+    public ArrayList preOrder(){
+        ArrayList res = new ArrayList();
+        if(root == null) return res;
+        TreeNode R = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while(R != null || !stack.empty()){
+            if(R != null){
+                res.add(R.val);
+                stack.push(R);
+                R = R.left;
+            }
+            else{
+                R = stack.pop().right;
+            }
+        }
+        return res;
+    }
+
+    public ArrayList inOrder(){
+        ArrayList res = new ArrayList();
+        if(root == null) return res;
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode R = root;
+        while(R != null || !stk.empty()){
+            if(R != null){
+                stk.push(R);
+                R = R.left;
+            }
+            else{
+                R = stk.pop();
+                res.add(R.val);
+                R = R.right;
+            }
+        }
+        return res;
+    }
+
+    public ArrayList postOrder(){
+        ArrayList res = new ArrayList();
+        if(root == null) return res;
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode R = root;
+        do{
+            while(R != null){
+                stk.push(R);
+                R = R.left;
+            }
+            TreeNode q=null;
+            while(!stk.empty()){
+                R = stk.peek();
+                if(R.right == q){
+                    res.add(R.val);
+                    stk.pop();
+                    q = R;
+                }
+                else{
+                    R = R.right;
+                    break;
+                }
+            }
+        }while(!stk.empty());
+        return res;
+    }
+
+
+}
---
+++ Array/src/test/org/sjtu/gkq/BtreeTest.java
@@ -0,0 +1,17 @@
+package org.sjtu.gkq;
+
+import java.util.ArrayList;
+
+public class BtreeTest {
+    public static void main(String[] args){
+        int a[] = {10, 3, 6, 8, 2, 4, 7, 5, 9, 1};
+        BinaryTree bt = new BinaryTree();
+        for(int i=0; i<a.length; i++){
+            bt.push(a[i]);
+        }
+        ArrayList res = bt.postOrder(bt.root);
+        for(int i=0; i<res.size(); i++){
+            System.out.print(res.get(i) + " ");
+        }
+    }
+}
