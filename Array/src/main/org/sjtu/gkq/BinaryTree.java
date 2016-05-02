419f22296277e4bd9041fc16f5de3393f449da09
gkq <gukunqi@gmai.com>
2016-05-02 21:42:03 +0800

    add some recusive tree algorithms

--- Array/src/main/org/sjtu/gkq/BinaryTree.java
+++ Array/src/main/org/sjtu/gkq/BinaryTree.java
@@ -1,9 +1,6 @@
 package org.sjtu.gkq;
 
-import javax.swing.tree.TreeNode;
 import java.util.*;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class BinaryTree {
@@ -153,4 +150,107 @@ public class BinaryTree {
         return res;
     }
 
+    public int maxDepth(TreeNode root){
+        if(root == null) return 0;
+        else
+            return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return root;
+        TreeNode tmp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tmp);
+        return root;
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)
+            return true;
+        if(p==null || q==null)
+            return false;
+        if(p.val == q.val)
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        else
+            return false;
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(p.val>root.val && q.val>root.val)
+            return lowestCommonAncestor(root.right, p, q);
+        else if(p.val<root.val && q.val<root.val)
+            return lowestCommonAncestor(root.left, p, q);
+        else
+            return root;
+    }
+
+    /**
+     * unique BST
+     * @param n
+     * @return
+     */
+    public int numTrees(int n) {
+        int[] f = new int[n+1];
+        f[0] = 1;
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=i;j++)
+                f[i] += f[j-1]*f[i-j];
+        return f[n];
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        if(root==null) return -1;
+        Stack<TreeNode> s = new Stack();
+        TreeNode p = root;
+        while(p!=null || !s.empty()){
+            if(p!=null){
+                s.push(p);
+                p = p.left;
+            }
+            else{
+                p = s.pop();
+                k--;
+                if(k==0) return p.val;
+                p = p.right;
+            }
+        }
+        return -1;
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length==0) return null;
+        return fun(nums,0,nums.length-1);
+    }
+
+    private TreeNode fun(int[] nums, int i, int j){
+        if(i>j) return null;
+        int mid = i+(j-i)/2;
+        TreeNode p = new TreeNode(nums[mid]);
+        p.left = fun(nums,i,mid-1);
+        p.right = fun(nums,mid+1,j);
+        return p;
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return isSymmetric(root.left,root.right);
+    }
+    private boolean isSymmetric(TreeNode p, TreeNode q){
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        return p.val==q.val && isSymmetric(p.left,q.right) && isSymmetric(q.left,p.right);
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return BalancedHeight(root)>=0;
+    }
+    private int BalancedHeight(TreeNode R){
+        if(R==null) return 0;
+        int left = BalancedHeight(R.left);
+        int right = BalancedHeight(R.right);
+        if(left<0 || right<0 || Math.abs(left-right)>1)
+            return -1;
+        return Math.max(left,right)+1;
+    }
 }
--- Array/src/test/org/sjtu/gkq/BtreeTest.java
+++ Array/src/test/org/sjtu/gkq/BtreeTest.java
@@ -6,12 +6,14 @@ public class BtreeTest {
     public static void main(String[] args){
         int a[] = {10, 3, 6, 8, 2, 4, 7, 5, 9, 1};
         BinaryTree bt = new BinaryTree();
-        for(int i=0; i<a.length; i++){
-            bt.push(a[i]);
-        }
+//        for(int i=0; i<a.length; i++){
+//            bt.push(a[i]);
+//        }
+        bt.root = bt.sortedArrayToBST(a);
         ArrayList res = bt.levelOrder();
         for(int i=0; i<res.size(); i++){
             System.out.print(res.get(i) + " ");
         }
+
     }
 }
